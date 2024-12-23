b97d5cfd7923e74d485aa9d24fb8deeff2f7782d
kurocifer <draculemihawk232@gmail.com>
2024-12-23 20:20:08 +0100

    snowlake pb soln 1

--- ch1/snowflakes/appch1/snowflakes.c
+++ ch1/snowflakes/appch1/snowflakes.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+
+#define SIZE 10000
+
 int identical_right(int snow1[], int snow2[], int start) {
     for(int offset = 0; offset < 6; offset++) {
         if(snow1[offset] != snow2[(start + offset) % 6])
@@ -30,7 +34,34 @@ int are_identical(int snow1[], int snow2[]) {
         if(identical_right(snow1, snow2, start))
             return 1;
     } 
-
     return 0;
+}
+
+void identify_idetical(int snowflakes[][6], int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(are_identical(snowflakes[i], snowflakes[j])) {
+                printf("Twin snowflakes found.\n");
+                return;
+            }
+        }
+    }
+    printf("No two snowflakes are alike.\n");
+}
+
+void main(void) {
+  static int snowflakes[SIZE][6];
+  int n, i, j; // n: number of snowflakes.
+
+  printf("number of snowlakes: ");
+  scanf("%d", &n);
+
+  for(i = 0; i < n; i++) {
+    printf("snowflake %d\n", i + 1);
+
+    for(j = 0; j < 6; j++)
+      scanf("%d", &snowflakes[i][j]);
+  }
+  identify_idetical(snowflakes, n);
 }
 
